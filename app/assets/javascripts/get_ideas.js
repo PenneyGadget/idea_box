528bb648433be3a573bf92d5facbf69d589e134b
PenneyGadget <dreamofgreen@gmail.com>
2016-03-21 18:06:51 -0600

    Truncates idea body

--- app/assets/javascripts/get_ideas.js
+++ app/assets/javascripts/get_ideas.js
@@ -5,7 +5,12 @@ function getIdeas() {
     success: function(ideas){
       $.each(ideas, function(index, idea){
         $('#latest-ideas').append(
-          "<div class='idea'><h3>" + idea.title + "</h3><p>" + idea.body + "</p></div>"
+          "<div class='idea'><h3>"
+          + idea.title + "</h3><p>"
+          + truncate(idea.body)
+          + "</p><p>Quality: "
+          + qualities[idea.quality]
+          + "</p></div>"
         );
       });
     }
---
+++ app/assets/javascripts/truncate.js
@@ -0,0 +1,7 @@
+function truncate(string) {
+  if(string.length > 100){
+    return string.substr(0, string.lastIndexOf(' ', 100)) + '...';
+  } else {
+    return string;
+  }
+}
