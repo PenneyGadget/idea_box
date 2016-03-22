51c4725e559d8e34b06c7ed96ee1afecb5b37a7d
PenneyGadget <dreamofgreen@gmail.com>
2016-03-21 18:35:28 -0600

    Ideas rendering in proper order

--- app/assets/javascripts/get_ideas.js
+++ app/assets/javascripts/get_ideas.js
@@ -1,18 +1,29 @@
-function getIdeas() {
+function getIdeas(){
+  var newestIdeaID = parseInt($(".idea").first().data("id"));
+
   $.ajax({
     type: 'GET',
     url: '/api/v1/ideas.json',
     success: function(ideas){
       $.each(ideas, function(index, idea){
-        $('#latest-ideas').append(
-          "<div class='idea'><h3>"
-          + idea.title + "</h3><p>"
-          + truncate(idea.body)
-          + "</p><p>Quality: "
-          + qualities[idea.quality]
-          + "</p></div>"
-        );
+        if (isNaN(newestIdeaID) || idea.id > newestIdeaID) {
+          renderIdea(idea);
+        }
       });
     }
   });
 }
+
+function renderIdea(idea){
+    $('#latest-ideas').prepend(
+      "<div class='idea' idea-id='" +
+      idea.id +
+      "'><h3>" +
+      idea.title +
+      "</h3><p>" +
+      truncate(idea.body) +
+      "</p><p>Quality: " +
+      qualities[idea.quality] +
+      "</div>"
+    );
+  }
--- app/assets/javascripts/truncate.js
+++ app/assets/javascripts/truncate.js
@@ -1,4 +1,4 @@
-function truncate(string) {
+function truncate(string){
   if(string.length > 100){
     return string.substr(0, string.lastIndexOf(' ', 100)) + '...';
   } else {
